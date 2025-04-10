1883b9619a964e25cef7fc52fcab26448c277e0a
mahendra <mahendras00b@gmail.com>
2025-04-10 14:34:12 +0530

    set orgin

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:[process.env.FRONTEND_URL ,'*'],
+    origin:'*',
     credentials:true
 }
 
